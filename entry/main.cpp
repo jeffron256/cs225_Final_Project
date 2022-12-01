64b0a16ea07725856cc72bc0762f1f123d8edf98
fj7 <fj7@illinois.edu>
2022-12-01 21:31:51 +0000

    main file altered

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,1 +1,9 @@
-// to be implemented
+#include <iostream>
+
+#include "mapMaker.h"
+
+int main()
+{
+    mapMaker m;
+    vector<vector<string>> tmp = m.file_to_Vec("bus_stop_info_weekday_.csv");
+}
